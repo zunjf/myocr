7557fb11ade475564310c23c9f2a1e68c6674e1b
handarbeni <handarbeni.irfan@gmail.com>
2017-07-19 13:57:51 +0700

    fixed code for saving and restoring ocr model

--- train_main.py
+++ train_main.py
@@ -4,54 +4,45 @@ from helper import model_ocr as model
 import numpy as np
 import config.default as config
 import random
-import os.path
+import sys
 
-data_train_val_dir = 'characters' # directory data
+def ocr_training(model_name):
+    data_train_val_dir = 'characters' # directory data
 
-data_set = imgloader.get_dataset(data_train_val_dir)
-imgtrain_list, lbltrain_list, imgval_list, lblval_list = imgloader.get_image_and_labels(data_set, 0.8, 0.2)
+    data_set = imgloader.get_dataset(data_train_val_dir)
+    imgtrain_list, lbltrain_list, _, _ = imgloader.get_image_and_labels(data_set, 0.8, 0.2)
 
-onehot = np.zeros((len(imgtrain_list), config.n_classes))
-onehot[np.arange(len(imgtrain_list)), lbltrain_list] = 1
-lbltrain_list = onehot
+    onehot = np.zeros((len(imgtrain_list), config.n_classes))
+    onehot[np.arange(len(imgtrain_list)), lbltrain_list] = 1
+    lbltrain_list = onehot
 
-# Randomize training data
-combined = list(zip(imgtrain_list, lbltrain_list))
-random.shuffle(combined)
-imgtrain_list, lbltrain_list = zip(*combined)
+    # Randomize training data
+    combined = list(zip(imgtrain_list, lbltrain_list))
+    random.shuffle(combined)
+    imgtrain_list, lbltrain_list = zip(*combined)
 
-# Labeling
-onehot = np.zeros((len(imgval_list), config.n_classes))
-onehot[np.arange(len(imgval_list)), lblval_list] = 1
-lblval_list = onehot
+    y = tf.nn.softmax(model.logits)
 
-#sess = tf.InteractiveSession()
-y = tf.nn.softmax(model.logits)
+    # Cross Entropy and accuracy
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=model.logits, labels=model.y_)
+    loss = tf.reduce_mean(cross_entropy)
 
-# Cross Entropy and accuracy
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=model.logits, labels=model.y_)
-loss = tf.reduce_mean(cross_entropy)
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(model.y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
-train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(model.y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"), name="op_to_restore")
+    # Summary
+    tf.summary.scalar('cross_entropy', loss)
+    tf.summary.scalar('accuracy', accuracy)
 
-# Summary
-tf.summary.scalar('cross_entropy', loss)
-tf.summary.scalar('accuracy', accuracy)
+    # Tensorboard
+    merged = tf.summary.merge_all()
 
-# Tensorboard
-merged = tf.summary.merge_all()
-#sess.run(tf.global_variables_initializer())
-#sess.run(tf.local_variables_initializer())
-test_data = imgloader.read_data(imgval_list, config.image_size)
-saver = tf.train.Saver()
+    # Create model saver
+    saver = tf.train.Saver()
 
-with tf.Session() as sess:
-    if os.path.isfile('example_model/checkpoint'):
-        saver = tf.train.import_meta_graph('example_model/model_ocr.meta')
-        saver.restore(sess, tf.train.latest_checkpoint('example_model/'))
-    else:
+    # Training
+    with tf.Session() as sess:
         itr = 0
         train_writer = tf.summary.FileWriter('train_board', sess.graph)
         sess.run(tf.global_variables_initializer())
@@ -65,11 +56,13 @@ with tf.Session() as sess:
                 batch_data = imgloader.read_data(batch_paths, config.image_size)
                 #print np.array(batch_data).shape
                 acc, l, _, sum_merged = sess.run([accuracy, loss, train_step, merged], feed_dict={model.x: batch_data,
-                                                               model.y_:batch_labels,
-                                                               model.keep_prob:0.5})
+                                                    model.y_:batch_labels,
+                                                    model.keep_prob:0.5})
 
                 itr +=1
                 train_writer.add_summary(sum_merged, itr)
                 print("epoch %d: %d/%d - loss: %.3f - acc: %.3f" % (i, step, steps, l, acc))
-        saver.save(sess, "example_model/model_ocr")
-    print('test accuracy %g' % accuracy.eval(feed_dict={model.x: test_data, model.y_: lblval_list, model.keep_prob: 1.0}))
+        saver.save(sess, "model/" + model_name)
+
+if __name__ == "__main__":
+    ocr_training(sys.argv[1])
